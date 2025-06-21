93efe95964d4df771da39f383340ff3b5fae221d
임도현 <162318258+limdodod@users.noreply.github.com>
2025-06-21 22:30:38 +0900

    [Bronze III] Title: 약수 구하기, Time: 40 ms, Memory: 32544 KB -BaekjoonHub

---
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.py
@@ -0,0 +1,13 @@
+N,K=map(int,input().split())
+num=[]
+
+for i in range(1,N+1):
+    if N%i==0:
+        num.append(i)
+
+if len(num)<K:
+    print(0)
+else:
+    print(num[K-1])
+        
+        

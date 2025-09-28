8c1b14a095b582b6187d4c3c783bfc4b34b0d738
임도현 <162318258+limdodod@users.noreply.github.com>
2025-09-28 16:10:09 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
@@ -0,0 +1,14 @@
+n=int(input())
+cnt=n
+
+for i in range(n):
+    word=input()
+
+    for j in range(len(word)-1):
+        if word[j]==word[j+1]:
+            continue
+        elif word[j] in word[j+1:]:
+            cnt-=1
+            break
+        
+print(cnt)
